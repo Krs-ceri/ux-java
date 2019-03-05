3e931ffafe87211ce2b4d4e062c951c48c4d5170
Nizar Rezaigui <nizar.rezaigui@alumni.univ-avignon.fr>
2019-03-05 11:23:56 +0100

    Image

--- src/application/Morpion.java
+++ src/application/Morpion.java
@@ -6,38 +6,38 @@ import java.util.*;
 public class Morpion {
 
 	private static int current = 0;
-	protected int plateau[][];
+	protected Tictactoe plateau[][];
 	private int playerX = 1;
 	private int playerY = 2;
 	
 	
 	public Morpion() {
-		this.plateau = new int[2][2];
-		for (int i = 0; i < plateau.length; i++) {
+		this.plateau = new Tictactoe[2][2];
+	/*	for (int i = 0; i < plateau.length; i++) {
 			for (int j = 0; j < plateau.length; j++) {
 				this.plateau[i][j] = -1;
 			}
-		}
+		}*/
 		
 	}
 	
-	public int winCondition() {
-		int winner = -1;		//Nobody win
-		if(this.plateau[0][0] != -1 ) {
+	public String winCondition() {
+		String winner = "";		//Nobody win
+		if(this.plateau[0][0] == Tictactoe.CIRCLE || this.plateau[0][0] == Tictactoe.CROSS) {
 			if((this.plateau[0][1] == this.plateau[0][0] && this.plateau[0][2] == this.plateau[0][0]) 
 					|| (this.plateau[1][0] == this.plateau[0][0] && this.plateau[2][0] == this.plateau[0][0]) 
-					|| (this.plateau[1][1] == this.plateau[0][0] && this.plateau[2][2] == this.plateau[0][0])) { winner = this.plateau[0][0]; }
+					|| (this.plateau[1][1] == this.plateau[0][0] && this.plateau[2][2] == this.plateau[0][0])) { winner = this.plateau[0][0].toString(); }
 	
 		}
-		if(this.plateau[2][2] != -1) {
+		if(this.plateau[0][0] == Tictactoe.CIRCLE || this.plateau[0][0] == Tictactoe.CROSS) {
 			if((this.plateau[2][1] == this.plateau[2][2] && this.plateau[0][2] == this.plateau[2][2]) 
 					|| (this.plateau[2][0] == this.plateau[2][2] && this.plateau[2][2] == this.plateau[1][2]) 
-					|| (this.plateau[1][1] == this.plateau[2][2] && this.plateau[2][2] == this.plateau[0][0])) { winner = this.plateau[2][2]; }
+					|| (this.plateau[1][1] == this.plateau[2][2] && this.plateau[2][2] == this.plateau[0][0])) { winner = this.plateau[2][2].toString(); }
 	
 		}
-		if(this.plateau[1][1] != -1) {
+		if(this.plateau[0][0] == Tictactoe.CIRCLE || this.plateau[0][0] == Tictactoe.CROSS) {
 			if((this.plateau[0][1] == this.plateau[1][1] && this.plateau[0][2] == this.plateau[2][1]) 
-					|| (this.plateau[1][0] == this.plateau[1][1] && this.plateau[1][1] == this.plateau[2][1])) { winner = this.plateau[1][1]; }
+					|| (this.plateau[1][0] == this.plateau[1][1] && this.plateau[1][1] == this.plateau[2][1])) { winner = this.plateau[1][1].toString(); }
 	
 		}
 
@@ -45,7 +45,7 @@ public class Morpion {
 	}
 	
 	public boolean addTac(int player, int x, int y) {
-		if(this.plateau[x][y] != -1 )	return false;
+		if(this.plateau[x][y] == Tictactoe.CIRCLE || this.plateau[x][y] == Tictactoe.CROSS)	return false;
 		this.plateau[x][y] = player;
 		return true;
 	}
